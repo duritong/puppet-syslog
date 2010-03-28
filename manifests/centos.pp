de75da61f8fff8f156047c98e60867f42ce9d908
mh <mh@immerda.ch>
2010-03-29 00:44:31 +0200

    new mailinglists

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -5,9 +5,9 @@ class syslog::centos inherits syslog::base {
 
     file{'/etc/sysconfig/syslog':
         source => [ "puppet://$server/modules/site-syslog/config/CentOS/${fqdn}/syslog", 
-                    "puppet://$server/modules/site-syslog/config/CentOS/syslog.${lsbdistrelease}", 
+                    "puppet://$server/modules/site-syslog/config/CentOS/syslog.${lsbmajdistrelease}", 
                     "puppet://$server/modules/site-syslog/config/CentOS/syslog", 
-                    "puppet://$server/modules/site-config/CentOS/syslog.${lsbdistrelease}", 
+                    "puppet://$server/modules/site-config/CentOS/syslog.${lsbmajdistrelease}", 
                     "puppet://$server/modules/syslog/config/CentOS/syslog" ],
         notify => Service['syslog'],
         owner => root, group => 0, mode => 0644;
