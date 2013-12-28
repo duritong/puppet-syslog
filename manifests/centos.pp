154ff92517e01ffe34e29b3a91c11c3249202a73
mh <mh@immerda.ch>
2013-12-28 16:48:35 +0100

    reduce dependency on lsb modules, rather use the builtin facts

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,15 +1,18 @@
+# syslog on centos
 class syslog::centos inherits syslog::base {
   Package[syslog]{
     name => 'sysklogd',
   }
 
   file{'/etc/sysconfig/syslog':
-    source => [ "puppet:///modules/site_syslog/config/CentOS/${::fqdn}/syslog", 
-                "puppet:///modules/site_syslog/config/CentOS/syslog.${::lsbmajdistrelease}", 
-                "puppet:///modules/site_syslog/config/CentOS/syslog", 
-                "puppet:///modules/site_config/CentOS/syslog.${::lsbmajdistrelease}", 
-                "puppet:///modules/syslog/config/CentOS/syslog" ],
-    notify => Service['syslog'],
-    owner => root, group => 0, mode => 0644;
+    source  => ["puppet:///modules/site_syslog/config/CentOS/${::fqdn}/syslog",
+                "puppet:///modules/site_syslog/config/CentOS/syslog.${::operatingsystemmajrelease}",
+                'puppet:///modules/site_syslog/config/CentOS/syslog',
+                "puppet:///modules/site_config/CentOS/syslog.${::operatingsystemmajrelease}",
+                'puppet:///modules/syslog/config/CentOS/syslog' ],
+    notify  => Service['syslog'],
+    owner   => root,
+    group   => 0,
+    mode    => '0644';
   }
 }
