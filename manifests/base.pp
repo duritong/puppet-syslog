59e91e8d75319ba94b3d2f2c9ca4884db1c5927d
mh <mh@immerda.ch>
2015-11-28 17:41:55 +0100

    linting for future parser

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,24 +1,27 @@
+# basic syslog stuff
 class syslog::base {
-  package{syslog:
+  package{'syslog':
     ensure => present,
   }
 
   file{'/etc/syslog.conf':
-    source => [ "puppet:///modules/site_syslog/config/${::fqdn}/syslog.conf", 
-                "puppet:///modules/site_syslog/config/${::domain}/syslog.conf",
-                "puppet:///modules/site_syslog/config/${::operatingsystem}/syslog.conf",
-                "puppet:///modules/site_syslog/config/syslog.conf", 
-                "puppet:///modules/syslog/config/${::operatingsystem}/syslog.conf",
-                "puppet:///modules/syslog/config/syslog.conf"],
-     notify => Service['syslog'],
-     require => Package['syslog'],
-     owner => root, group => 0, mode => 0644;
+    source  => [ "puppet:///modules/site_syslog/config/${::fqdn}/syslog.conf",
+                  "puppet:///modules/site_syslog/config/${::domain}/syslog.conf",
+                  "puppet:///modules/site_syslog/config/${::operatingsystem}/syslog.conf",
+                  'puppet:///modules/site_syslog/config/syslog.conf',
+                  "puppet:///modules/syslog/config/${::operatingsystem}/syslog.conf",
+                  'puppet:///modules/syslog/config/syslog.conf'],
+    notify  => Service['syslog'],
+    require => Package['syslog'],
+    owner   => root,
+    group   => 0,
+    mode    => '0644';
   }
 
-  service{syslog:
-    ensure => running,
-    enable => true,
+  service{'syslog':
+    ensure    => running,
+    enable    => true,
     hasstatus => true,
-    require => Package['syslog'],
+    require   => Package['syslog'],
   }
 }
